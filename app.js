8436f925dc1b6db5a8adbca8ae222f191c530178
07ASN <HASAN.OMAR.20002@GMAIL.COM>
2025-01-07 16:56:57 +0300

    Soltion Complate

--- app.js
+++ app.js
@@ -1,3 +1,58 @@
+// Add Event Form
+document.getElementById('order-form').addEventListener('submit', handleForm);
+
+// Load Orders | DOMContentLoaded = When the Document is Fully Loaded
+document.addEventListener('DOMContentLoaded', loadOrdersFromLocalStorage);
+
+//Orders to View it on Cards
+let orders = [];
+
+// Handle Form
+function handleForm(e) {
+  e.preventDefault();
+
+  // Collect Form Fields Values
+  const fullName = document.getElementById('fullName').value;
+  const password = document.getElementById('password').value;
+  const dob = document.getElementById('dob').value;
+  const gender = document.getElementById('gender').value;
+  const phone = document.getElementById('phone').value;
+
+  if (/\s/.test(fullName)) {
+    window.alert("UserName should be without whitespaces")
+    return;
+  }
+
+  if (!/^(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*])[A-Za-z\d!@#$%^&*]{8,}$/.test(password)) {
+    window.alert("Password should be at least [8 digits][1 uppercase][1 special characters]");
+    return;
+  }
+
+  if (!/^07\d{8}$/.test(phone)){
+    window.alert("Phone number must be 10 digits long and start with '07'.");
+    return 
+  }
+  // Collect Checkbox Values
+  const orderType = [];
+  if (document.getElementById('shawarma').checked) orderType.push('Shawarma');
+  if (document.getElementById('zinger').checked)   orderType.push('Zinger');
+  if (document.getElementById('burger').checked)   orderType.push('Burger');
+
+  // Collect Radio Value
+  const orderOption = document.querySelector('input[name="orderOption"]:checked').value;
+
+  const imageUrl = "customer.png.png";
+
+  const customer = new Customer(fullName, password, dob, gender, phone, orderType, orderOption, imageUrl);
+  orders.push(customer);
+
+  // Save the Updated OrderArray to LocalStorage
+  localStorage.setItem('orders', JSON.stringify(orders));
+
+  // Display the New Order Card
+  renderOrders();
+}
+
 // Customer Constructor
 function Customer(fullName, password, dob, gender, phone, orderType, orderOption, imageUrl) {
   this.fullName = fullName;
@@ -10,13 +65,8 @@ function Customer(fullName, password, dob, gender, phone, orderType, orderOption
   this.imageUrl = imageUrl;
 }
 
-//Customer Orders Information to View it on Cards
-let orders = [];
-
-
 //Displaying Cards
 function renderOrders() {
-  //Select div
   const ordersList = document.getElementById('orders-list');
 
   // Clear innerHTML from previous cards
@@ -47,46 +97,9 @@ function renderOrders() {
     
     ordersList.appendChild(orderCard);
   }
-  
 }
 
-// Function to Handle the Form
-function handleFormSubmission(e) {
-  e.preventDefault();
-
-  // Collect Form Field Values
-  const fullName = document.getElementById('fullName').value;
-  const password = document.getElementById('password').value;
-  const dob = document.getElementById('dob').value;
-  const gender = document.getElementById('gender').value;
-  const phone = document.getElementById('phone').value;
-
-  // Collect Checkbox Values
-  const orderType = [];
-  if (document.getElementById('shawarma').checked) orderType.push('Shawarma');
-  if (document.getElementById('zinger').checked)   orderType.push('Zinger');
-  if (document.getElementById('burger').checked)   orderType.push('Burger');
-
-  // Collect Radio Value
-  const orderOption = document.querySelector('input[name="orderOption"]:checked').value;
-
-  // Image URL (Static)
-  const imageUrl = "customer.png.png";
-
-  // Create Object with Collected Values
-  const customer = new Customer(fullName, password, dob, gender, phone, orderType, orderOption, imageUrl);
-
-  // Add the Customer Object to the Orders Array
-  orders.push(customer);
-
-  // Save Updated Orders Array to LocalStorage
-  localStorage.setItem('orders', JSON.stringify(orders));
-
-  // Display Old Cards
-  renderOrders();
-}
-
-// Get Cards From LocalStorge
+// Fetches Cards From LocalStorge
 function loadOrdersFromLocalStorage() {
   const storedOrders = JSON.parse(localStorage.getItem('orders'));
   if (storedOrders) {
@@ -94,9 +107,3 @@ function loadOrdersFromLocalStorage() {
     renderOrders();        // Display the Loaded Orders
   }
 }
-
-// Add event listener for Form
-document.getElementById('order-form').addEventListener('submit', handleFormSubmission);
-
-// Load Orders from localStorage | DOMContentLoaded = When the Document is Fully Loaded
-document.addEventListener('DOMContentLoaded', loadOrdersFromLocalStorage);
